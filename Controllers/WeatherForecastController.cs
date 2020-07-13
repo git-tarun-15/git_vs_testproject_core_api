36b4466cf3375126e6df4e596f88a752936256ff
git-tarun-15 <tarunkumar.chatterjee@gmail.com>
2020-07-13 18:24:20 -0400

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace git_vs_testproject_core_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Test1","Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
